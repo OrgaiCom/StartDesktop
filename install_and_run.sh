1c005ecdedf5cc8646e480e99dcbd9f05c6fc346
OrgaiCom <ryouko789+github@gmail.com>
2026-01-12 16:23:05 +0900

    Add files via upload

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 chmod +x install-chrome.sh
 chmod +x install-gemini.sh
+chmod +x install-lxterminal.sh
 chmod +x run-chrome.sh
 chmod +x start-lxqt.sh
 
 # ./install-chrome.sh
 ./install-gemini.sh
+# ./install-lxterminal.sh
 ./start-lxqt.sh
